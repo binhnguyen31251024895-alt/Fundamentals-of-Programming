c1c52301644e956881077fa1d1c297195c4cdf3d
LENOVO <LENOVO@NGUYENHOANGYBIN>
2026-06-01 20:58:39 +0700

    Them bai tap 6

---
+++ Fundamentals of Programming/Buoi 4/Bai tap # buoi 4.cs
@@ -0,0 +1,314 @@
+﻿//using System;
+//using System.Text;
+
+//class Program
+//{
+//    // Bai 1: Tinh tong hai so nguyen
+//    static int TinhTong(int a, int b)
+//    {
+//        return a + b;
+//    }
+
+//    // Bai 2: Kiem tra so chan le
+//    static bool KiemTraChan(int n)
+//    {
+//        return n % 2 == 0;
+//    }
+
+//    // Bai 3: Tim so lon nhat trong ba so
+//    static int TimMax(int a, int b, int c)
+//    {
+//        return Math.Max(Math.Max(a, b), c);
+//    }
+
+//    // Bai 4: Tinh giai thua
+//    static long TinhGiaiThua(int n)
+//    {
+//        long ketQua = 1;
+
+//        for (int i = 1; i <= n; i++)
+//        {
+//            ketQua *= i;
+//        }
+
+//        return ketQua;
+//    }
+
+//    // Bai 5: Dao nguoc chuoi
+//    static string DaoNguocChuoi(string input)
+//    {
+//        char[] arr = input.ToCharArray();
+//        Array.Reverse(arr);
+//        return new string(arr);
+//    }
+
+//    // Bai 6: Kiem tra so nguyen to
+//    static bool KiemTraNguyenTo(int n)
+//    {
+//        if (n < 2)
+//            return false;
+
+//        for (int i = 2; i <= Math.Sqrt(n); i++)
+//        {
+//            if (n % i == 0)
+//                return false;
+//        }
+
+//        return true;
+//    }
+
+//    // Bai 7: In day Fibonacci
+//    static void InFibonacci(int n)
+//    {
+//        int a = 0;
+//        int b = 1;
+
+//        for (int i = 0; i < n; i++)
+//        {
+//            Console.Write(a + " ");
+
+//            int temp = a + b;
+//            a = b;
+//            b = temp;
+//        }
+
+//        Console.WriteLine();
+//    }
+
+//    // Bai 8: Dem so luong nguyen am trong chuoi
+//    static int DemNguyenAm(string s)
+//    {
+//        int dem = 0;
+//        s = s.ToLower();
+
+//        foreach (char c in s)
+//        {
+//            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+//            {
+//                dem++;
+//            }
+//        }
+
+//        return dem;
+//    }
+
+//    // Bai 9: Tinh luy thua khong dung Math.Pow
+//    static double TinhLuyThua(double x, int y)
+//    {
+//        double ketQua = 1;
+
+//        for (int i = 1; i <= Math.Abs(y); i++)
+//        {
+//            ketQua *= x;
+//        }
+
+//        if (y < 0)
+//            return 1 / ketQua;
+
+//        return ketQua;
+//    }
+
+//    // Bai 10: Tinh diem trung binh cua mang
+//    static double TinhTrungBinh(int[] arr)
+//    {
+//        int tong = 0;
+
+//        foreach (int x in arr)
+//        {
+//            tong += x;
+//        }
+
+//        return (double)tong / arr.Length;
+//    }
+
+//    // Bai 11: Kiem tra chuoi doi xung
+//    static bool KiemTraDoiXung(string s)
+//    {
+//        int left = 0;
+//        int right = s.Length - 1;
+
+//        while (left < right)
+//        {
+//            if (s[left] != s[right])
+//                return false;
+
+//            left++;
+//            right--;
+//        }
+
+//        return true;
+//    }
+
+//    // Bai 12: Chuyen do C sang do F
+//    static double CelsiusToFahrenheit(double c)
+//    {
+//        return c * 9 / 5 + 32;
+//    }
+
+//    // Bai 13: Tim gia tri nho nhat trong mang
+//    static int TimMin(int[] arr)
+//    {
+//        int min = arr[0];
+
+//        foreach (int x in arr)
+//        {
+//            if (x < min)
+//            {
+//                min = x;
+//            }
+//        }
+
+//        return min;
+//    }
+
+//    // Bai 14: Tinh tong cac chu so cua mot so nguyen
+//    static int TongCacChuSo(int n)
+//    {
+//        n = Math.Abs(n);
+//        int tong = 0;
+
+//        while (n > 0)
+//        {
+//            tong += n % 10;
+//            n /= 10;
+//        }
+
+//        return tong;
+//    }
+
+//    // Bai 15: Sap xep mang tang dan
+//    static void SapXepMang(int[] arr)
+//    {
+//        Array.Sort(arr);
+
+//        foreach (int x in arr)
+//        {
+//            Console.Write(x + " ");
+//        }
+
+//        Console.WriteLine();
+//    }
+
+//    // Bai 16: Xoa ky tu trung lap
+//    static string XoaTrungLap(string s)
+//    {
+//        string ketQua = "";
+
+//        foreach (char c in s)
+//        {
+//            if (!ketQua.Contains(c))
+//            {
+//                ketQua += c;
+//            }
+//        }
+
+//        return ketQua;
+//    }
+
+//    // Bai 17: Tim uoc chung lon nhat bang thuat toan Euclid
+//    static int UCLN(int a, int b)
+//    {
+//        a = Math.Abs(a);
+//        b = Math.Abs(b);
+
+//        while (b != 0)
+//        {
+//            int temp = b;
+//            b = a % b;
+//            a = temp;
+//        }
+
+//        return a;
+//    }
+
+//    // Bai 18: Chuyen doi thap phan sang nhi phan
+//    static string DecimalToBinary(int n)
+//    {
+//        if (n == 0)
+//            return "0";
+
+//        string binary = "";
+
+//        while (n > 0)
+//        {
+//            binary = (n % 2) + binary;
+//            n /= 2;
+//        }
+
+//        return binary;
+//    }
+
+//    // Bai 19: Kiem tra nam nhuan
+//    static bool KiemTraNamNhuan(int year)
+//    {
+//        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+//    }
+
+//    // Bai 20: Dem so tu trong cau
+//    static int DemSoTu(string sentence)
+//    {
+//        string[] words = sentence.Split(
+//            new char[] { ' ' },
+//            StringSplitOptions.RemoveEmptyEntries
+//        );
+
+//        return words.Length;
+//    }
+
+//    // Ham Main de chay thu toan bo bai
+//    static void Mainbuoi4(string[] args) { }
+//    {
+//        Console.OutputEncoding = Encoding.UTF8;
+
+//        Console.WriteLine("BAI TAP C# - HAM FUNCTIONS");
+//        Console.WriteLine("--------------------------");
+
+//        Console.WriteLine("Bai 1: Tong 5 + 3 = " + TinhTong(5, 3));
+
+//        Console.WriteLine("Bai 2: 10 la so chan? " + KiemTraChan(10));
+
+//        Console.WriteLine("Bai 3: Max cua 4, 9, 2 = " + TimMax(4, 9, 2));
+
+//        Console.WriteLine("Bai 4: 5! = " + TinhGiaiThua(5));
+
+//        Console.WriteLine("Bai 5: Dao chuoi 'Hello' = " + DaoNguocChuoi("Hello"));
+
+//        Console.WriteLine("Bai 6: 7 la so nguyen to? " + KiemTraNguyenTo(7));
+//        Console.WriteLine("Bai 6: 10 la so nguyen to? " + KiemTraNguyenTo(10));
+
+//        Console.Write("Bai 7: 6 so Fibonacci dau tien: ");
+//        InFibonacci(6);
+
+//        Console.WriteLine("Bai 8: So nguyen am trong 'Hello World' = " + DemNguyenAm("Hello World"));
+
+//        Console.WriteLine("Bai 9: 2^3 = " + TinhLuyThua(2, 3));
+
+//        int[] arr10 = { 4, 5, 6, 7 };
+//        Console.WriteLine("Bai 10: Trung binh cua [4, 5, 6, 7] = " + TinhTrungBinh(arr10));
+
+//        Console.WriteLine("Bai 11: 'radar' doi xung? " + KiemTraDoiXung("radar"));
+//        Console.WriteLine("Bai 11: 'hello' doi xung? " + KiemTraDoiXung("hello"));
+
+//        Console.WriteLine("Bai 12: 25 do C = " + CelsiusToFahrenheit(25) + " do F");
+
+//        int[] arr13 = { 10, 5, 8, 2, 9 };
+//        Console.WriteLine("Bai 13: Min cua [10, 5, 8, 2, 9] = " + TimMin(arr13));
+
+//        Console.WriteLine("Bai 14: Tong chu so cua 1234 = " + TongCacChuSo(1234));
+
+//        int[] arr15 = { 3, 1, 4, 2 };
+//        Console.Write("Bai 15: Mang sau khi sap xep: ");
+//        SapXepMang(arr15);
+
+//        Console.WriteLine("Bai 16: Xoa trung lap 'programming' = " + XoaTrungLap("programming"));
+
+//        Console.WriteLine("Bai 17: UCLN cua 12 va 18 = " + UCLN(12, 18));
+
+//        Console.WriteLine("Bai 18: 10 doi sang nhi phan = " + DecimalToBinary(10));
+
+//        Console.WriteLine("Bai 19: 2024 la nam nhuan? " + KiemTraNamNhuan(2024));
+//        Console.WriteLine("Bai 19: 2023 la nam nhuan? " + KiemTraNamNhuan(2023));
+
+//        Console.WriteLine("Bai 20: So tu trong cau = " + DemSoTu("Hoc lap trinh C# rat thu vi"));
+//    }
+//}
---
+++ Fundamentals of Programming/Chu de 5/Bai tap 6#.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Text;
+
+class Baitap6
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("===== PART 1 =====");
+
+        int[] arr = CreateRandomArray(10, 1, 20);
+
+        Console.Write("Random array: ");
+        PrintArray(arr);
+
+        Console.WriteLine("Average value: " + CalculateAverage(arr));
+
+        Console.Write("Enter a value to test if array contains it: ");
+        int valueToTest = int.Parse(Console.ReadLine());
+
+        if (ContainsValue(arr, valueToTest))
+            Console.WriteLine("The array contains " + valueToTest);
+        else
+            Console.WriteLine("The array does not contain " + valueToTest);
+
+        Console.Write("Enter a value to find its index: ");
+        int valueToFind = int.Parse(Console.ReadLine());
+
+        int index = FindIndex(arr, valueToFind);
+
+        if (index != -1)
+            Console.WriteLine("Index of " + valueToFind + " is: " + index);
+        else
+            Console.WriteLine(valueToFind + " is not found in the array.");
+
+        Console.Write("Enter a value to remove from array: ");
+        int valueToRemove = int.Parse(Console.ReadLine());
+
+        int[] removedArray = RemoveElement(arr, valueToRemove);
+
+        Console.Write("Array after removing " + valueToRemove + ": ");
+        PrintArray(removedArray);
+
+        Console.WriteLine("Maximum value: " + FindMaximum(arr));
+        Console.WriteLine("Minimum value: " + FindMinimum(arr));
+
+        int[] reversedArray = ReverseArray(arr);
+
+        Console.Write("Reversed array: ");
+        PrintArray(reversedArray);
+
+        int[] duplicateValues = FindDuplicateValues(arr);
+
+        Console.Write("Duplicate values: ");
+        PrintArray(duplicateValues);
+
+        int[] noDuplicateArray = RemoveDuplicateElements(arr);
+
+        Console.Write("Array after removing duplicates: ");
+        PrintArray(noDuplicateArray);
+
+
+        Console.WriteLine();
+        Console.WriteLine("===== PART 2 - QUESTION 1: BUBBLE SORT =====");
+
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("Enter integer number " + (i + 1) + ": ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Original array: ");
+        PrintArray(numbers);
+
+        BubbleSort(numbers);
+
+        Console.Write("Sorted array: ");
+        PrintArray(numbers);
+
+
+        Console.WriteLine();
+        Console.WriteLine("===== PART 2 - QUESTION 2: LINEAR SEARCH WORD =====");
+
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        Console.Write("Enter a word to search: ");
+        string word = Console.ReadLine();
+
+        bool found = LinearSearchWord(sentence, word);
+
+        if (found)
+            Console.WriteLine("The word appears in the sentence.");
+        else
+            Console.WriteLine("The word does not appear in the sentence.");
+
+        Console.WriteLine();
+        Console.WriteLine("Program finished.");
+    }
+
+    static int[] CreateRandomArray(int size, int minValue, int maxValue)
+    {
+        Random random = new Random();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(minValue, maxValue + 1);
+        }
+
+        return arr;
+    }
+
+    static double CalculateAverage(int[] arr)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return (double)sum / arr.Length;
+    }
+
+    static bool ContainsValue(int[] arr, int value)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == value)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static int FindIndex(int[] arr, int value)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == value)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    static int[] RemoveElement(int[] arr, int value)
+    {
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] != value)
+            {
+                count++;
+            }
+        }
+
+        int[] newArray = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] != value)
+            {
+                newArray[index] = arr[i];
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+
+    static int FindMaximum(int[] arr)
+    {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    static int FindMinimum(int[] arr)
+    {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+
+    static int[] ReverseArray(int[] arr)
+    {
+        int[] reversed = new int[arr.Length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            reversed[i] = arr[arr.Length - 1 - i];
+        }
+
+        return reversed;
+    }
+
+    static int[] FindDuplicateValues(int[] arr)
+    {
+        int[] temp = new int[arr.Length];
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            bool isDuplicate = false;
+
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (isDuplicate && !ContainsValueInPart(temp, count, arr[i]))
+            {
+                temp[count] = arr[i];
+                count++;
+            }
+        }
+
+        int[] duplicates = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            duplicates[i] = temp[i];
+        }
+
+        return duplicates;
+    }
+
+    static int[] RemoveDuplicateElements(int[] arr)
+    {
+        int[] temp = new int[arr.Length];
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!ContainsValueInPart(temp, count, arr[i]))
+            {
+                temp[count] = arr[i];
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = temp[i];
+        }
+
+        return result;
+    }
+
+    static bool ContainsValueInPart(int[] arr, int length, int value)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (arr[i] == value)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = 0; j < arr.Length - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static bool LinearSearchWord(string sentence, string word)
+    {
+        char[] separators = { ' ', '.', ',', '!', '?', ';', ':', '-', '(', ')', '"', '\'' };
+
+        string[] words = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].ToLower() == word.ToLower())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            Console.WriteLine("Empty array");
+            return;
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
